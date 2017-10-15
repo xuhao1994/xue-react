be8c2791d3557ce470dce584eb35372994169763
XueMary <632565970@qq.com>
2017-10-15 15:05:16 +0800

    add antd

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,9 +1,10 @@
 import React,{PureComponent} from 'react';
+import { Button } from 'antd';
 import './Home.css';
 import * as listAction from '../../components/List/ListRedux';
 
 import fetch from '../../fetch';
-import connect from '../../redux/connect'; 
+import connect from '../../redux/connect';
 
 
 class Home extends PureComponent{
@@ -16,6 +17,7 @@ class Home extends PureComponent{
     return(
       <div className='home'>
         Home
+        <Button type="primary">Button</Button>
       </div>
     )
   }
